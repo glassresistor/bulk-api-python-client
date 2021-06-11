1e964d17081f822bb682e6bbc7761d77bc7a3c85
Mikela <glassresistor@gmail.com>
2021-06-11 12:46:34 -0700

    lazy load env_client so docs work (#177)

--- bulk_api_client/env_client.py
+++ bulk_api_client/env_client.py
@@ -1,17 +1,29 @@
 import os
+import sys
 
 from bulk_api_client.client import Client
 from bulk_api_client.exceptions import BulkAPIError
 
-token = os.getenv("BULK_API_TOKEN")
-if not token:
-    raise BulkAPIError("Environment variable BULK_API_TOKEN was not found.")
 
-api_url = os.getenv("BULK_API_URL")
+def _env_client():
+    token = os.getenv("BULK_API_TOKEN")
+    if not token:
+        raise BulkAPIError("Environment variable BULK_API_TOKEN was not found.")
 
-expiration_time = os.getenv("BULK_API_EXPIRATION_TIME")
+    api_url = os.getenv("BULK_API_URL")
 
-if expiration_time:
-    expiration_time = int(expiration_time)
+    expiration_time = os.getenv("BULK_API_EXPIRATION_TIME")
 
-env_client = Client(token, api_url=api_url, expiration_time=expiration_time)
+    if expiration_time:
+        expiration_time = int(expiration_time)
+
+    env_client = Client(token, api_url=api_url, expiration_time=expiration_time)
+    return env_client
+
+
+def __getattr__(name):
+    if name == "env_client":
+        return _env_client()
+
+    # Implicit else
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
